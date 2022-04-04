b8e6d41ee18800290aa56c1b8a7f6ddf81028f3e
harrislukman13 <harrislukman13@gmail.com>
2022-04-04 15:51:32 +0800

    home ui

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:ionicons/ionicons.dart';
 import 'package:upm_mii/pages/home/company_list.dart';
 import 'package:upm_mii/pages/home/insurance_plan_list.dart';
+import 'package:upm_mii/constants/style.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -17,8 +19,255 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Home'),
+    Size size = MediaQuery.of(context).size;
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          HeaderWithSearchBox(size: size),
+          const SizedBox(
+            height: 15,
+          ),
+          const BillSubcription(),
+          const SizedBox(
+            height: 15,
+          ),
+          const TitleWithbutton(text: "News"),
+          Container(
+            height: 100,
+            child: Card(
+              margin: const EdgeInsets.symmetric(horizontal: 20),
+              color: Colors.blue[100],
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: const [
+                        Text("data"),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          const TitleWithbutton(text: "Your Plans"),
+        ],
+      ),
+    );
+  }
+}
+
+class TitleWithbutton extends StatelessWidget {
+  const TitleWithbutton({
+    Key? key,
+    required this.text,
+  }) : super(key: key);
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        alignment: Alignment.topLeft,
+        height: 24,
+        child: Stack(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 20),
+              child: Text(text,
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold, fontSize: 15)),
+            ),
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                margin: const EdgeInsets.only(right: 10 / 4),
+                height: 7,
+              ),
+            ),
+          ],
+        ));
+  }
+}
+
+class BillSubcription extends StatelessWidget {
+  const BillSubcription({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 65,
+      child: Card(
+        margin: const EdgeInsets.symmetric(horizontal: 20),
+        color: Colors.blue[800],
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: const [
+                  Expanded(
+                    child: Text(
+                      "Total Amount Due",
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w700,
+                          color: Colors.white),
+                    ),
+                  ),
+                  Text(
+                    "RM 81.00",
+                    style: TextStyle(
+                        fontStyle: FontStyle.italic, color: Colors.white),
+                  )
+                ],
+              ),
+              const SizedBox(height: 5),
+              const Text(
+                "as of 10 april 2022",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class HeaderWithSearchBox extends StatelessWidget {
+  const HeaderWithSearchBox({
+    Key? key,
+    required this.size,
+  }) : super(key: key);
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    return Container(
+      margin: const EdgeInsets.only(bottom: 20),
+      height: (2 / 10 * height) + 30,
+      child: Stack(
+        children: [
+          Container(
+            height: 2 / 10 * height,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Color(0xff243E82),
+                  Color(0xff00AAE0),
+                ],
+              ),
+              //borderRadius: BorderRadius.only(
+              // bottomLeft: Radius.circular(36),
+              // bottomRight: Radius.circular(36)),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text(
+                          'InsureME',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24,
+                          ),
+                        ),
+                        Text(
+                          '"To save you in time of need"',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const CircleAvatar(
+                      radius: 40,
+                      backgroundImage: AssetImage('assets/logo.png'),
+                    ),
+                  ],
+                ),
+                const Text(
+                  'Welcome Back! Amirah',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 16,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              margin: const EdgeInsets.symmetric(horizontal: 20),
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              height: 54,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(
+                    offset: const Offset(0, 10),
+                    blurRadius: 50,
+                    color: Colors.blue.withOpacity(0.23),
+                  ),
+                ],
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                    hintText: "Search",
+                    hintStyle: TextStyle(
+                      color: Colors.grey.withOpacity(0.5),
+                    ),
+                    enabledBorder: InputBorder.none,
+                    focusedBorder: InputBorder.none,
+                    prefixIcon: Icon(
+                      Ionicons.search_outline,
+                      color: Colors.blue,
+                    )),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
