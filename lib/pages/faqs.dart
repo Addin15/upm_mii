62962fa1c43393a8284b2852f753514527d5c87a
XiaoZik <81950435+XiaoZik@users.noreply.github.com>
2022-04-05 17:07:26 +0800

    faq
    faqs updated

--- lib/pages/faqs.dart
+++ lib/pages/faqs.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:upm_mii/constants/style.dart';
+import 'package:expandable/expandable.dart';
 
+// ignore_for_file: prefer_const_constructors
 class FAQs extends StatefulWidget {
   const FAQs({Key? key}) : super(key: key);
 
@@ -12,8 +15,10 @@ class _FAQsState extends State<FAQs> {
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
-    return Container(
-      child: Container(
+
+    return Column(children: [
+      Container(
+          child: Container(
         height: 2 / 10 * height,
         decoration: const BoxDecoration(
           gradient: LinearGradient(
@@ -76,7 +81,84 @@ class _FAQsState extends State<FAQs> {
             ),
           ],
         ),
+      ) //replace,
+          ),
+      SizedBox(
+        height: 10,
       ),
-    );
+      TextFormField(
+        decoration: Style.searchStyle(hintText: 'Search keywords'),
+      ),
+      SizedBox(
+        height: 10,
+      ),
+      Padding(
+        padding: EdgeInsets.all(10.0),
+        child: ExpandableTheme(
+          data: ExpandableThemeData(
+              iconColor: Color.fromARGB(255, 102, 102, 102),
+              animationDuration: const Duration(milliseconds: 500)),
+          child: Column(
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 242, 242, 242),
+                    border:
+                        Border.all(color: Color.fromARGB(255, 242, 242, 242)),
+                    borderRadius: BorderRadius.all(Radius.circular(20))),
+                child: ExpandablePanel(
+                  header: Container(
+                      padding: EdgeInsets.fromLTRB(10, 10, 5, 5),
+                      child: Text(
+                        'What is Life Insurance?',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Color.fromARGB(255, 102, 102, 102)),
+                      )),
+                  collapsed: Container(
+                      padding: EdgeInsets.fromLTRB(10, 10, 5, 5),
+                      child: Text(
+                        'Life insurance is a contract between you and an insurance company. Essentially, in exchange for your premium payments, the insurance company will pay a lump sum known as a death benefit to your beneficiaries after your death. Your beneficiaries can use the money for whatever purpose they choose.',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Color.fromARGB(255, 0, 0, 0)),
+                      )),
+                  expanded: SizedBox.shrink(),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 242, 242, 242),
+                    border:
+                        Border.all(color: Color.fromARGB(255, 242, 242, 242)),
+                    borderRadius: BorderRadius.all(Radius.circular(20))),
+                child: ExpandablePanel(
+                  header: Container(
+                      padding: EdgeInsets.fromLTRB(10, 10, 5, 5),
+                      child: Text(
+                        'Why is life insurance useful?',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Color.fromARGB(255, 102, 102, 102)),
+                      )),
+                  collapsed: Container(
+                      padding: EdgeInsets.fromLTRB(10, 10, 5, 5),
+                      child: Text(
+                        'Life cover is useful to ensure the financial stability of your family in case you are unable to earn due to an accident or illness. The policy also pays the benefits to your beneficiaries in case of an untoward event. Procuring such coverage ensures that your family can to meet their expenses and sustain their lifestyles even in your absence.',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Color.fromARGB(255, 0, 0, 0)),
+                      )),
+                  expanded: SizedBox.shrink(),
+                ),
+              )
+            ],
+          ),
+        ),
+      )
+    ]);
   }
 }
