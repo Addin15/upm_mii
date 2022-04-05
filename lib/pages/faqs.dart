de3d2ddebfb80ccd6b556baed7910e2e7108f0e7
XiaoZik <81950435+XiaoZik@users.noreply.github.com>
2022-04-06 00:07:40 +0800

    Update faqs.dart
    Improve structure of the code by implementing for loop

--- lib/pages/faqs.dart
+++ lib/pages/faqs.dart
@@ -10,25 +10,31 @@ class FAQs extends StatefulWidget {
   State<FAQs> createState() => _FAQsState();
 }
 
+class Data {
+  String topics;
+  String contents;
+
+  Data(this.topics, this.contents);
+}
+
 class _FAQsState extends State<FAQs> {
   @override
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
-    List topics = [
-      'Glossary',
-      'What is Life Insurance',
-      'Why is Life Insurance useful',
-      'Is Life Insurance necessary',
-      'How do I decide on the amount of Life Insurance cover I need?'
+    var dataList = [
+      Data('Glossary',
+          'Insurer : The Life Insurance Company.\n\nBenificiaries : The people receiving the money from the Life Insurance Policy in the case of your death.\n\nLife Insurance Policy : A guarantee the insurer pays a sum of money to named beneficiaries when the insured dies in exchange for the premiums paid by the policyholder during their lifetime.\n\nPremium : The payment made to the Life Insurance Company in exchange for your Life Insurance Policy.\n\nMaturity Benefits :  A lump-sum amount the insurance company pays you after the maturity of the Life Insurance Policy. This means that if your Life Insurance Policy is for a term of 15 years, the Life Insurance Policy matures after 15 years. '),
+      Data('What is Life Insurance',
+          'Life insurance is a contract between you and an insurance company. Essentially, in exchange for your premium payments, the insurance company will pay a lump sum known as a death benefit to your beneficiaries after your death. Your beneficiaries can use the money for whatever purpose they choose.'),
+      Data('Why is Life Insurance useful',
+          'Life insurance is useful to ensure the financial stability of your family in case you are unable to earn due to an accident or illness. The policy also pays the benefits to your beneficiaries in case of an untoward event. Procuring such coverage ensures that your family can to meet their expenses and sustain their lifestyles even in your absence.'),
+      Data('Is Life Insurance necessary',
+          'One of the things to know about life insurance is that while it is not necessary, purchasing a policy is a smart investment decision. This is especially if you have dependents such as spouse, parents, and children. The life plan will provide financial security to your family if you are not around. Moreover, life policies offer several benefits and are a flexible instrument. Some of these include the flexibility of adding riders for greater coverage or withdrawing part of the accumulated corpus to meet expenses such as children’s education or wedding.'),
+      Data('How do I decide on the amount of Life Insurance cover I need?',
+          'The maturity benefits primarily depend on the premium you pay during the policy term. This amount depends on several factors such as your lifestyle, spending habits, income, expenses, and debt obligations. It is recommended you procure coverage that is approximately between eight-ten times of your annual income.')
     ];
-    List contents = [
-      'Insurer : The Life Insurance Company.\n\nBenificiaries : The people receiving the money from the Life Insurance Policy in the case of your death.\n\nLife Insurance Policy : A guarantee the insurer pays a sum of money to named beneficiaries when the insured dies in exchange for the premiums paid by the policyholder during their lifetime.\n\nPremium : The payment made to the Life Insurance Company in exchange for your Life Insurance Policy.\n\nMaturity Benefits :  A lump-sum amount the insurance company pays you after the maturity of the Life Insurance Policy. This means that if your Life Insurance Policy is for a term of 15 years, the Life Insurance Policy matures after 15 years. ',
-      'Life insurance is a contract between you and an insurance company. Essentially, in exchange for your premium payments, the insurance company will pay a lump sum known as a death benefit to your beneficiaries after your death. Your beneficiaries can use the money for whatever purpose they choose.',
-      'Life insurance is useful to ensure the financial stability of your family in case you are unable to earn due to an accident or illness. The policy also pays the benefits to your beneficiaries in case of an untoward event. Procuring such coverage ensures that your family can to meet their expenses and sustain their lifestyles even in your absence.',
-      'One of the things to know about life insurance is that while it is not necessary, purchasing a policy is a smart investment decision. This is especially if you have dependents such as spouse, parents, and children. The life plan will provide financial security to your family if you are not around. Moreover, life policies offer several benefits and are a flexible instrument. Some of these include the flexibility of adding riders for greater coverage or withdrawing part of the accumulated corpus to meet expenses such as children’s education or wedding.',
-      'The maturity benefits primarily depend on the premium you pay during the policy term. This amount depends on several factors such as your lifestyle, spending habits, income, expenses, and debt obligations. It is recommended you procure coverage that is approximately between eight-ten times of your annual income.'
-    ];
+
     return SingleChildScrollView(
       child: Column(children: [
         Container(
@@ -118,184 +124,47 @@ class _FAQsState extends State<FAQs> {
                   height: 395,
                   child: ListView(
                     children: <Widget>[
-                      Container(
-                        decoration: BoxDecoration(
-                            color: Color.fromARGB(255, 242, 242, 242),
-                            border: Border.all(
-                                color: Color.fromARGB(255, 242, 242, 242)),
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(20))),
-                        child: ExpandableNotifier(
-                          child: ScrollOnExpand(
-                            child: ExpandablePanel(
-                              header: Container(
-                                  padding: EdgeInsets.fromLTRB(10, 10, 5, 5),
-                                  child: Text(
-                                    'Glossary',
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        color:
-                                            Color.fromARGB(255, 102, 102, 102)),
-                                  )),
-                              collapsed: SizedBox.shrink(),
-                              expanded: Container(
-                                  padding: EdgeInsets.fromLTRB(10, 10, 5, 5),
-                                  child: Text(
-                                    'Insurer : The Life Insurance Company.\n\nBenificiaries : The people receiving the money from the Life Insurance Policy in the case of your death.\n\nLife Insurance Policy : A guarantee the insurer pays a sum of money to named beneficiaries when the insured dies in exchange for the premiums paid by the policyholder during their lifetime.\n\nPremium : The payment made to the Life Insurance Company in exchange for your Life Insurance Policy.\n\nMaturity Benefits :  A lump-sum amount the insurance company pays you after the maturity of the Life Insurance Policy. This means that if your Life Insurance Policy is for a term of 15 years, the Life Insurance Policy matures after 15 years. ',
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        color: Color.fromARGB(255, 0, 0, 0)),
-                                  )),
+                      for (var data in dataList)
+                        Column(children: <Widget>[
+                          Container(
+                            decoration: BoxDecoration(
+                                color: Color.fromARGB(255, 242, 242, 242),
+                                border: Border.all(
+                                    color: Color.fromARGB(255, 242, 242, 242)),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(20))),
+                            child: ExpandableNotifier(
+                              child: ScrollOnExpand(
+                                child: ExpandablePanel(
+                                  header: Container(
+                                      padding:
+                                          EdgeInsets.fromLTRB(10, 10, 5, 5),
+                                      child: Text(
+                                        data.topics,
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            color: Color.fromARGB(
+                                                255, 102, 102, 102)),
+                                      )),
+                                  collapsed: SizedBox.shrink(),
+                                  expanded: Container(
+                                      padding:
+                                          EdgeInsets.fromLTRB(10, 10, 5, 5),
+                                      child: Text(
+                                        data.contents,
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            color:
+                                                Color.fromARGB(255, 0, 0, 0)),
+                                      )),
+                                ),
+                              ),
                             ),
                           ),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Container(
-                        decoration: BoxDecoration(
-                            color: Color.fromARGB(255, 242, 242, 242),
-                            border: Border.all(
-                                color: Color.fromARGB(255, 242, 242, 242)),
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(20))),
-                        child: ExpandableNotifier(
-                          child: ScrollOnExpand(
-                            child: ExpandablePanel(
-                              header: Container(
-                                  padding: EdgeInsets.fromLTRB(10, 10, 5, 5),
-                                  child: Text(
-                                    'What is Life Insurance?',
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        color:
-                                            Color.fromARGB(255, 102, 102, 102)),
-                                  )),
-                              collapsed: SizedBox.shrink(),
-                              expanded: Container(
-                                  padding: EdgeInsets.fromLTRB(10, 10, 5, 5),
-                                  child: Text(
-                                    'Life insurance is a contract between you and an insurance company. Essentially, in exchange for your premium payments, the insurance company will pay a lump sum known as a death benefit to your beneficiaries after your death. Your beneficiaries can use the money for whatever purpose they choose.',
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        color: Color.fromARGB(255, 0, 0, 0)),
-                                  )),
-                            ),
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Container(
-                        decoration: BoxDecoration(
-                            color: Color.fromARGB(255, 242, 242, 242),
-                            border: Border.all(
-                                color: Color.fromARGB(255, 242, 242, 242)),
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(20))),
-                        child: ExpandableNotifier(
-                          child: ScrollOnExpand(
-                            scrollOnExpand: true,
-                            scrollOnCollapse: false,
-                            child: ExpandablePanel(
-                              header: Container(
-                                  padding: EdgeInsets.fromLTRB(10, 10, 5, 5),
-                                  child: Text(
-                                    'Why is Life Insurance useful?',
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        color:
-                                            Color.fromARGB(255, 102, 102, 102)),
-                                  )),
-                              collapsed: SizedBox.shrink(),
-                              expanded: Container(
-                                  padding: EdgeInsets.fromLTRB(10, 10, 5, 5),
-                                  child: Text(
-                                    'Life cover is useful to ensure the financial stability of your family in case you are unable to earn due to an accident or illness. The policy also pays the benefits to your beneficiaries in case of an untoward event. Procuring such coverage ensures that your family can to meet their expenses and sustain their lifestyles even in your absence.',
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        color: Color.fromARGB(255, 0, 0, 0)),
-                                  )),
-                            ),
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Container(
-                        decoration: BoxDecoration(
-                            color: Color.fromARGB(255, 242, 242, 242),
-                            border: Border.all(
-                                color: Color.fromARGB(255, 242, 242, 242)),
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(20))),
-                        child: ExpandableNotifier(
-                          child: ScrollOnExpand(
-                            scrollOnExpand: true,
-                            scrollOnCollapse: false,
-                            child: ExpandablePanel(
-                              header: Container(
-                                  padding: EdgeInsets.fromLTRB(10, 10, 5, 5),
-                                  child: Text(
-                                    'Is Life Insurance necessary?',
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        color:
-                                            Color.fromARGB(255, 102, 102, 102)),
-                                  )),
-                              collapsed: SizedBox.shrink(),
-                              expanded: Container(
-                                  padding: EdgeInsets.fromLTRB(10, 10, 5, 5),
-                                  child: Text(
-                                    'One of the things to know about life insurance is that while it is not necessary, purchasing a policy is a smart investment decision. This is especially if you have dependents such as spouse, parents, and children. The life plan will provide financial security to your family if you are not around. Moreover, life policies offer several benefits and are a flexible instrument. Some of these include the flexibility of adding riders for greater coverage or withdrawing part of the accumulated corpus to meet expenses such as children’s education or wedding.',
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        color: Color.fromARGB(255, 0, 0, 0)),
-                                  )),
-                            ),
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Container(
-                        decoration: BoxDecoration(
-                            color: Color.fromARGB(255, 242, 242, 242),
-                            border: Border.all(
-                                color: Color.fromARGB(255, 242, 242, 242)),
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(20))),
-                        child: ExpandableNotifier(
-                          child: ScrollOnExpand(
-                            scrollOnExpand: true,
-                            scrollOnCollapse: false,
-                            child: ExpandablePanel(
-                              header: Container(
-                                  padding: EdgeInsets.fromLTRB(10, 10, 5, 5),
-                                  child: Text(
-                                    'How do I decide on the amount of Life Insurance cover I need?',
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        color:
-                                            Color.fromARGB(255, 102, 102, 102)),
-                                  )),
-                              collapsed: SizedBox.shrink(),
-                              expanded: Container(
-                                  padding: EdgeInsets.fromLTRB(10, 10, 5, 5),
-                                  child: Text(
-                                    'The maturity benefits primarily depend on the premium you pay during the policy term. This amount depends on several factors such as your lifestyle, spending habits, income, expenses, and debt obligations. It is recommended you procure coverage that is approximately between eight-ten times of your annual income.',
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        color: Color.fromARGB(255, 0, 0, 0)),
-                                  )),
-                            ),
-                          ),
-                        ),
-                      )
+                          SizedBox(
+                            height: 10,
+                          )
+                        ]),
                     ],
                   ),
                 )
