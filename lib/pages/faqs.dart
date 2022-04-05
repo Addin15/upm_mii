351f7613cadadc531501e5600f3f8a21fe96868d
XiaoZik <81950435+XiaoZik@users.noreply.github.com>
2022-04-05 14:15:09 +0800

    Update faqs.dart

--- lib/pages/faqs.dart
+++ lib/pages/faqs.dart
@@ -10,8 +10,73 @@ class FAQs extends StatefulWidget {
 class _FAQsState extends State<FAQs> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('FAQs'),
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    return Container(
+      child: Container(
+        height: 2 / 10 * height,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color(0xff243E82),
+              Color(0xff00AAE0),
+            ],
+          ),
+          //borderRadius: BorderRadius.only(
+          // bottomLeft: Radius.circular(36),
+          // bottomRight: Radius.circular(36)),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text(
+                      'InsureME',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 24,
+                      ),
+                    ),
+                    Text(
+                      '"To save you in time of need"',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                ),
+                const CircleAvatar(
+                  radius: 40,
+                  backgroundImage: AssetImage('assets/logo.png'),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            const Text(
+              'Frequently Asked Questions',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.w600,
+                fontSize: 25,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
